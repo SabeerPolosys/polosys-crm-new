29e7987acfbf60e988e59c2dd7ff5379bbeb1ed1
SabeerPolosys <sabeerpolosys@gmail.com>
2026-03-17 16:04:31 +0530

    Added reset in customer filter page.

--- app/(dashboard)/customers/page.tsx
+++ app/(dashboard)/customers/page.tsx
@@ -56,6 +56,7 @@ export default function Customer() {
     setOrganizationName("");
     setStartFilterType("eq");
     setEndFilterType("eq");
+    setCustomerDetails(allCustomers);
   };
   useEffect(() => {
     const getAllCountries = async () => {
